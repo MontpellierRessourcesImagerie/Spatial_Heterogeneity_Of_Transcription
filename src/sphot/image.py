9c5fda6dd5bae143b20885add4c68d6db3cfe940
volker-baecker <volker.baecker@mri.cnrs>
2025-04-15 18:23:11 +0200

    added nn-stats

--- src/sphot/image.py
+++ src/sphot/image.py
@@ -1,13 +1,18 @@
+import math
+import random
+import numpy as np
 from skimage.segmentation import clear_border
 from skimage.morphology import remove_small_objects
 from skimage.segmentation import relabel_sequential
+from bigfish import detection
+from scipy.spatial import KDTree
+from scipy.stats import ecdf
 
 
 
 class Segmentation:
-    ''' Segment the cells of the embryon and create a label images with the labels 1 to N for the N
-    cells in the image that are not touching the borders.
-    '''
+    """ Segment the cells of the embryon and create a label images with the labels 1 to N for the N
+    cells in the image that are not touching the borders."""
 
 
     def __init__(self, image):
@@ -29,17 +34,19 @@ class Segmentation:
 
 
     def run(self):
+        yield
         self.runCellpose()
-        print(self.labels.shape)
+        yield
         if self.clearBorder:
             self.labels = clear_border(self.labels)
-        print(self.labels.shape)
+        yield
         if self.minSize > 0:
             self.labels = remove_small_objects(self.labels, min_size=self.minSize)
-        print(self.labels.shape)
+        yield
         if self.clearBorder or self.minSize>0:
             self.labels, _, _ = relabel_sequential(self.labels)
-        print(self.labels.shape)
+        yield
+
 
 
     def runCellpose(self):
@@ -55,4 +62,127 @@ class Segmentation:
                                        z_axis=0,
                                        stitch_threshold=self.stitchThreshold)
         self.labels = labels
+
+
+
+class SpotDetection:
+
+
+    def __init__(self, image):
+        self.image = image
+        self.spots = None
+        self.threshold = 4
+        self.shallRemoveDuplicates = True
+        self.shallFindThreshold = False
+        self.scale = (50, 50, 50)
+        self.spotRadius = 150
+
+
+    def run(self):
+        yield
+        self.spots = detection.detect_spots(
+            self.image,
+            threshold=self.threshold,
+            remove_duplicate=self.shallRemoveDuplicates,
+            return_threshold=self.shallFindThreshold,
+            voxel_size=self.scale,
+            spot_radius=self.spotRadius)
+        yield
+
+
+
+class SpotPerCellAnalyzer:
+
+
+    def __init__(self, points, labels, scale):
+        self.points = points
+        self.labels = labels
+        self.maxLabel = np.max(labels)
+        self.pointsPerCell = {}
+        self.nnDistances = {}
+        self.scale = scale
+        self.ecdfs = {}
+        self.envelops = {}
+
+
+    def run(self):
+        self._calculateSpotsPerCell()
+        self.nnDistances = self.getNNDistances()
+        self.calculateECDFs()
+
+
+    def _calculateSpotsPerCell(self):
+        maxLabel = int(np.max(self.labels))
+        for i in range(0, maxLabel + 1):
+            self.pointsPerCell[i] = []
+        for point in self.points:
+            label = self.labels[point[0], point[1], point[2]]
+            self.pointsPerCell[label].append(point)
+
+
+    def getNNDistances(self):
+        nnDistances = {}
+        for label in range(1, self.maxLabel + 1):
+            data = self.pointsPerCell[label]
+            nnDistances[label] = self.getNNDistancesFor(data)
+        return nnDistances
+
+
+    def getNNDistancesFor(self, data):
+        kdtree = KDTree(data)
+        dist, points = kdtree.query(data, 2)
+        nnDistances = (dist[:,1] * self.scale, points)
+        return nnDistances
+
+
+    def calculateECDFs(self):
+        self.ecdfs = {}
+        for label in range(1, self.maxLabel + 1):
+            self.ecdfs[label] = self.getECDF(self.nnDistances[label][0])
+
+
+    def getRandomPointsForLabel(self, label):
+        nrOfPoints = len(self.pointsPerCell[label])
+        label = np.ndarray.copy(self.labels)
+        label[label < label] = 0
+        label[label > label] = 0
+        nz = np.nonzero(label)
+        coords = np.transpose(nz)
+        maxSample = len(coords) - 1
+        indices = random.sample(range(maxSample + 1), nrOfPoints)
+        randPoints = coords[indices]
+        return randPoints
+
+
+    @classmethod
+    def getECDF(cls, distances):
+        res = ecdf(distances)
+        return res
+
+
+    def getEnvelopFor(self, label, nrOfSamples=100):
+        lower95thIndex = (5 * nrOfSamples) // 100
+        upper95thIndex = (95 * nrOfSamples) // 100
+        maxDist = np.max(self.nnDistances[label][0])
+        xValues = np.array(list(range(0, math.floor(maxDist+1), self.scale)))
+        envelops = np.zeros((nrOfSamples, len(xValues)))
+        for i in range(nrOfSamples):
+            points = self.getRandomPointsForLabel(label)
+            distances = self.getNNDistancesFor(points)[0]
+            cdf = ecdf(distances)
+            row = cdf.cdf.evaluate(xValues)
+            row = row.tolist()
+            for index, element in enumerate(row):
+                envelops[i, index] = element
+        maxEnvs = np.zeros(len(xValues))
+        minEnvs = np.zeros(len(xValues))
+        upper95ths = np.zeros(len(xValues))
+        lower95ths = np.zeros(len(xValues))
+        for i in range(len(xValues)):
+            column = np.sort(envelops[:, i])
+            minEnvs[i] = column[0]
+            lower95ths[i] = column[lower95thIndex]
+            upper95ths[i] = column[upper95thIndex]
+            maxEnvs[i] = column[nrOfSamples-1]
+        return minEnvs, lower95ths, maxEnvs, upper95ths
 
