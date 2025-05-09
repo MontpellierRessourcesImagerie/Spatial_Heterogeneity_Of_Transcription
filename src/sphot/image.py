acb18399ae87a8fd10fe5dcc45911c60a3933582
volker-baecker <volker.baecker@mri.cnrs>
2025-05-09 17:49:31 +0200

    added F- and H-function

--- src/sphot/image.py
+++ src/sphot/image.py
@@ -7,11 +7,12 @@ from skimage.segmentation import relabel_sequential
 from bigfish import detection
 from scipy.spatial import KDTree
 from scipy.stats import ecdf
+from scipy.spatial.distance import cdist
 
 
 
 class Segmentation:
-    """ Segment the cells of the embryon and create a label images with the labels 1 to N for the N
+    """ Segment the cells of the embryo and create a label images with the labels 1 to N for the N
     cells in the image that are not touching the borders."""
 
 
@@ -100,15 +101,32 @@ class SpotPerCellAnalyzer:
         self.maxLabel = np.max(labels)
         self.pointsPerCell = {}
         self.nnDistances = {}
+        self.allDistances = {}
         self.scale = scale
-        self.ecdfs = {}
-        self.envelops = {}
+        self.nnEcdfs = {}
+        self.adEcdfs = {}
+        self.esEcdfs = {}
+        self.nrOfEmptySpacePoints = 1000
+        self.emptySpacePointsPerCell = {}
+        self.emptySpaceDistances = {}
 
 
-    def run(self):
+    def calculateGFunction(self):
         self._calculateSpotsPerCell()
         self.nnDistances = self.getNNDistances()
-        self.calculateECDFs()
+        self.calculateNNECDFs()
+
+
+    def calculateHFunction(self):
+        self._calculateSpotsPerCell()
+        self.allDistances = self.getAllDistances()
+        self.calculateAllECDFs()
+
+
+    def calculateFFunction(self):
+        self._calculateSpotsPerCell()
+        self.emptySpaceDistances = self.getEmptySpaceDistances()
+        self.calculateEmptySpaceECDFs()
 
 
     def _calculateSpotsPerCell(self):
@@ -135,21 +153,68 @@ class SpotPerCellAnalyzer:
         return nnDistances
 
 
-    def calculateECDFs(self):
-        self.ecdfs = {}
+    def getEmptySpaceDistances(self):
+        emptySpaceDistances = {}
         for label in range(1, self.maxLabel + 1):
-            self.ecdfs[label] = self.getECDF(self.nnDistances[label][0])
+            data = self.pointsPerCell[label]
+            self.emptySpacePointsPerCell[label] = self.getNRandomPointsForLabel(label, self.nrOfEmptySpacePoints)
+            emptySpaceDistances[label] = self.getEmptySpaceDistancesFor(data, self.emptySpacePointsPerCell[label])
+        return emptySpaceDistances
+
+
+    def getEmptySpaceDistancesFor(self, data, refPoints):
+        kdtree = KDTree(data)
+        dist, points = kdtree.query(refPoints, 2)
+        nnDistances = (dist[:, 1] * self.scale, points)
+        return nnDistances
+
+
+    def getAllDistances(self):
+        allDistances = {}
+        for label in range(1, self.maxLabel + 1):
+            data = self.pointsPerCell[label]
+            allDistances[label] = self.getAllDistancesFor(data)
+        return allDistances
+
+
+    def getAllDistancesFor(self, data):
+        N = len(data)
+        dist = cdist(data, data, 'euclidean')
+        allDistances = (dist[np.triu_indices(N, 1)] * self.scale, data)
+        return allDistances
+
+
+    def calculateNNECDFs(self):
+        self.nnEcdfs = {}
+        for label in range(1, self.maxLabel + 1):
+            self.nnEcdfs[label] = self.getECDF(self.nnDistances[label][0])
+
+
+    def calculateAllECDFs(self):
+        self.adEcdfs = {}
+        for label in range(1, self.maxLabel + 1):
+            self.adEcdfs[label] = self.getECDF(self.allDistances[label][0])
+
+
+    def calculateEmptySpaceECDFs(self):
+        self.esEcdfs = {}
+        for label in range(1, self.maxLabel + 1):
+            self.esEcdfs[label] = self.getECDF(self.emptySpaceDistances[label][0])
 
 
     def getRandomPointsForLabel(self, label):
         nrOfPoints = len(self.pointsPerCell[label])
-        label = np.ndarray.copy(self.labels)
-        label[label < label] = 0
-        label[label > label] = 0
-        nz = np.nonzero(label)
+        return self.getNRandomPointsForLabel(label, nrOfPoints)
+
+
+    def getNRandomPointsForLabel(self, label, N):
+        labelsForLabel = np.ndarray.copy(self.labels)
+        labelsForLabel[labelsForLabel < label] = 0
+        labelsForLabel[labelsForLabel > label] = 0
+        nz = np.nonzero(labelsForLabel)
         coords = np.transpose(nz)
         maxSample = len(coords) - 1
-        indices = random.sample(range(maxSample + 1), nrOfPoints)
+        indices = random.sample(range(maxSample + 1), N)
         randPoints = coords[indices]
         return randPoints
 
@@ -160,15 +225,51 @@ class SpotPerCellAnalyzer:
         return res
 
 
-    def getEnvelopFor(self, label, nrOfSamples=100):
+    def getEnvelopForNNDistances(self, label, nrOfSamples=100):
+        maxDist = np.max(self.nnDistances[label][0])
+        return self.getEnvelopForDistanceFunction(label, self.getNNDistancesFor, maxDist, nrOfSamples)
+
+
+    def getEnvelopForAllDistances(self, label, nrOfSamples=100):
+        maxDist = np.max(self.allDistances[label][0])
+        return self.getEnvelopForDistanceFunction(label, self.getAllDistancesFor, maxDist, nrOfSamples)
+
+
+    def getEnvelopForEmptySpaceDistances(self, label, nrOfSamples=100):
+        maxDist = np.max(self.allDistances[label][0])
         lower95thIndex = (5 * nrOfSamples) // 100
         upper95thIndex = (95 * nrOfSamples) // 100
-        maxDist = np.max(self.nnDistances[label][0])
+        xValues = np.array(list(range(0, math.floor(maxDist + 1), self.scale)))
+        envelops = np.zeros((nrOfSamples, len(xValues)))
+        for i in range(nrOfSamples):
+            points = self.getRandomPointsForLabel(label)
+            distances = self.getEmptySpaceDistancesFor(points, self.emptySpacePointsPerCell[label])[0]
+            cdf = ecdf(distances)
+            row = cdf.cdf.evaluate(xValues)
+            row = row.tolist()
+            for index, element in enumerate(row):
+                envelops[i, index] = element
+        maxEnvs = np.zeros(len(xValues))
+        minEnvs = np.zeros(len(xValues))
+        upper95ths = np.zeros(len(xValues))
+        lower95ths = np.zeros(len(xValues))
+        for i in range(len(xValues)):
+            column = np.sort(envelops[:, i])
+            minEnvs[i] = column[0]
+            lower95ths[i] = column[lower95thIndex]
+            upper95ths[i] = column[upper95thIndex]
+            maxEnvs[i] = column[nrOfSamples - 1]
+        return minEnvs, lower95ths, upper95ths, maxEnvs
+
+
+    def getEnvelopForDistanceFunction(self, label, distanceFunction, maxDist, nrOfSamples=100):
+        lower95thIndex = (5 * nrOfSamples) // 100
+        upper95thIndex = (95 * nrOfSamples) // 100
         xValues = np.array(list(range(0, math.floor(maxDist+1), self.scale)))
         envelops = np.zeros((nrOfSamples, len(xValues)))
         for i in range(nrOfSamples):
             points = self.getRandomPointsForLabel(label)
-            distances = self.getNNDistancesFor(points)[0]
+            distances = distanceFunction(points)[0]
             cdf = ecdf(distances)
             row = cdf.cdf.evaluate(xValues)
             row = row.tolist()
@@ -185,4 +286,3 @@ class SpotPerCellAnalyzer:
             upper95ths[i] = column[upper95thIndex]
             maxEnvs[i] = column[nrOfSamples-1]
         return minEnvs, lower95ths, upper95ths, maxEnvs
-
